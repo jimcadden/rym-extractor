53726deef6a19868d6d1e46eb6945e3922a19d71
Jim Cadden <jmcadden@bu.edu>
2016-03-25 22:32:49 -0400

    Add day-of-year-released pipeline

--- rym/pipelines.py
+++ rym/pipelines.py
@@ -1,4 +1,5 @@
 from scrapy.exceptions import DropItem
+import datetime
 
 
 class CleanData(object):
@@ -12,7 +13,8 @@ class CleanData(object):
             item[key] = ''.join(val)
           else:
             item[key] = ', '.join(val)
-    item['release_date'] = item['release_date'].replace(',','')
+    if item['release_date'] is not None:
+      item['release_date'] = item['release_date'].replace(',','')
     return item
 
 class StringToInt(object):
@@ -24,4 +26,19 @@ class StringToInt(object):
           item[key] = float(item[key]) 
         else:
           item[key] = int(item[key].replace(',', '')) 
+    return item
+
+class DayOfYear(object):
+  def process_item(self, item, spider):
+    # Assumes item data is cleaned & strings converted to ints 
+    # release_date string may be: None, "January", "January 30"
+    month = 1
+    day = 1
+    months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    if item['release_date'] is not None:
+      parts = item['release_date'].split(' ')
+      month = months.index(parts[0]) + 1
+      if len(parts) == 2:
+        day = int(parts[1])
+    item['release_DOY'] = datetime.date(item['release_year'], month, day).timetuple().tm_yday
     return item
